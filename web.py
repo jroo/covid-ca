9304a994583f79c8a26cbcacd50903299dd35024
Joshua Ruihley <ruihley@gmail.com>
2020-04-24 14:37:11 -0400

    add ontario json endpoint

--- web.py
+++ web.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import jsonify, Flask, Response
+from models import *
+from playhouse.shortcuts import model_to_dict
 
 
 app = Flask(__name__)
@@ -7,3 +9,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/ontario.json')
+def ontario():
+    query = Daily.select().where(Daily.region == "Ontario")
+
+    s = []
+    for row in query:
+        s.append(model_to_dict(row))
+
+    return jsonify(s)
