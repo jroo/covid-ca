c4ef637db17359522e2f593f02996ef9b69a8466
Joshua Ruihley <ruihley@gmail.com>
2020-04-24 07:55:38 -0400

    chanaged update interval to 30 minutes PEP8 cleanup

--- scraper.py
+++ scraper.py
@@ -6,11 +6,13 @@ import wget
 print ('Hello Ontario scraper')
 print ('Listening for changes to %s' % os.environ['ONTARIO_STATUS_URL'])
 
-#check for update every 30 minutes
+
+# check for update every 30 minutes
 def check_url():
-	print ("Checking %s" % time.ctime())
-	threading.Timer(3000, check_url).start()
-	wget.download(os.environ['ONTARIO_STATUS_URL'], 'temp/ontario.csv')
+    print ("Checking %s" % time.ctime())
+    threading.Timer(1800, check_url).start()
+    wget.download(os.environ['ONTARIO_STATUS_URL'], 'temp/ontario.csv')
+
 
 if __name__ == '__main__':
-	check_url()
+    check_url()
