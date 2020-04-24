4f1a6c061651633c986c54465e6029086f03ee18
Joshua Ruihley <ruihley@gmail.com>
2020-04-24 09:03:09 -0400

    add model save csv to db

---
+++ models.py
@@ -0,0 +1,27 @@
+import os
+from peewee import *
+from playhouse.db_url import connect
+
+db = connect(os.environ['DATABASE_URL'])
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Daily(BaseModel):
+    primary_key = CompositeKey('region', 'report_date')
+    region = CharField()
+    report_date = DateTimeField()
+    confirmed_positive = IntegerField(null=True)
+    resolved = IntegerField(null=True)
+    deaths = IntegerField(null=True)
+    total_cases = IntegerField(null=True)
+    tests_past_day = IntegerField(null=True)
+    under_investigation = IntegerField(null=True)
+    hospitalizations = IntegerField(null=True)
+    icu = IntegerField(null=True)
+    icu_ventilator = IntegerField(null=True)
+
+
+db.create_tables([Daily])
--- web.py
+++ web.py
@@ -1,6 +1,5 @@
-import os
-
 from flask import Flask
+from models import *
 from peewee import *
 from playhouse.db_url import connect
 
@@ -10,27 +9,3 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
-db = connect(os.environ['DATABASE_URL'])
-
-
-class BaseModel(Model):
-    class Meta:
-        database = db
-
-
-class Daily(BaseModel):
-    region = CharField()
-    report_date = DateTimeField()
-    confirmed_positive = IntegerField(null=True)
-    resolved = IntegerField(null=True)
-    deaths = IntegerField(null=True)
-    TotalCases = IntegerField(null=True)
-    TestsPastDay = IntegerField(null=True)
-    UnderInvestigation = IntegerField(null=True)
-    Hospitalizations = IntegerField(null=True)
-    ICU = IntegerField(null=True)
-    ICUVentilator = IntegerField(null=True)
-
-
-db.create_tables([Daily])
