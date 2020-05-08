9f1d0f912afe802f6ea9fcd9f9b4073169788731
Joshua Ruihley <49064+jroo@users.noreply.github.com>
2020-05-08 17:54:36 -0400

    add inline graphs

--- static/chart.js
+++ static/chart.js
@@ -1,64 +1,79 @@
+const gw = 400
+const gh = 200
+const marginBottom = 10
+const marginDefault = 40
+
 // select svg container first
 
-const svg = d3.select('.canvas')
-	.append('svg')
-	.attr('height', 600)
-	.attr('width', 600);
+function drawGraph(div, dataField) {
 
-const margin = {'top': 20, 'right': 20, 'bottom': 100, 'left': 100};
-const graphWidth = 500 - margin.left - margin.right;
-const graphHeight = 500 - margin.top - margin.bottom;
+	const svg = d3.select('#canvas-' + div)
+		.append('svg')
+		.attr('height', gh)
+		.attr('width', gw);
 
-const graph = svg.append('g')
-	.attr('width', graphWidth)
-	.attr('height', graphHeight)
-	.attr('transform', `translate(${margin.left}, ${margin.top})`);
+	const margin = {'top': marginDefault, 'right': marginDefault, 'bottom': marginBottom, 'left': marginDefault};
+	const graphWidth = gw - margin.left - margin.right;
+	const graphHeight = gh - margin.top - margin.bottom;
 
-const xAxisGroup = graph.append('g')
-	.attr('transform', `translate(0, ${graphHeight})`)
-const yAxisGroup = graph.append('g');
+	const graph = svg.append('g')
+		.attr('width', graphWidth)
+		.attr('height', graphHeight)
+		.attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-d3.json('daily.json').then( data => {
+	const xAxisGroup = graph.append('g')
+		.attr('transform', `translate(0, ${graphHeight})`)
+	const yAxisGroup = graph.append('g');
 
-	const y = d3.scaleLinear()
-		.domain([0, d3.max(data, d => d.tests_past_day)])
-		.range([graphHeight, 0]);
+	d3.json('daily.json').then( data => {
 
-	const x = d3.scaleBand()
-		.domain(data.map(item => item.report_date))
-		.range([0, graphWidth])
-		.paddingInner(0.1)
-		.paddingOuter(0.2);
+		const y = d3.scaleLinear()
+			.domain([0, d3.max(data, d => _.get(d, dataField))])
+			.range([graphHeight, 0]);
 
-	// join data to rects
-	const rects = graph.selectAll('rect')
-		.data(data)
-		.attr('width', x.bandwidth)
-		.attr('height', d => graphHeight - y(d.tests_past_day))
-		.attr('fill', 'blue')
-		.attr('x', d => x(d.report_date))
-		.attr('y', d => y(d.tests_past_day));
+		const x = d3.scaleBand()
+			.domain(data.map(item => item.report_date))
+			.range([0, graphWidth])
+			.paddingInner(0.1)
+			.paddingOuter(0.2);
 
-
-	// append enter selection to the dom
-	rects.enter()
-		.append('rect')
+		// join data to rects
+		const rects = graph.selectAll('rect')
+			.data(data)
 			.attr('width', x.bandwidth)
-			.attr('height', d => graphHeight - y(d.tests_past_day))
-			.attr('fill', 'orange')
+			.attr('height', d => graphHeight - y(_.get(d, dataField)))
+			.attr('fill', '#00ccff')
 			.attr('x', d => x(d.report_date))
-			.attr('y', d => y(d.tests_past_day));
+			.attr('y', d => y(_.get(d, dataField)));
 
-	// create axis
-	const xAxis = d3.axisBottom(x)
-		.ticks(5);
-	const yAxis = d3.axisLeft(y)
-		.ticks(5)
-	xAxisGroup.call(xAxis);
-	yAxisGroup.call(yAxis);
-	xAxisGroup.selectAll('text')
-		.attr('transform', 'rotate(-48)')
-		.attr('text-anchor', 'end')
-		.attr('fill', 'grey');
 
-})
+		// append enter selection to the dom
+		rects.enter()
+			.append('rect')
+				.attr('width', x.bandwidth)
+				.attr('height', d => graphHeight - y(_.get(d, dataField)))
+				.attr('fill', '#00ccff')
+				.attr('x', d => x(d.report_date))
+				.attr('y', d => y(_.get(d, dataField)));
+
+		// create axis
+		const xAxis = d3.axisBottom(x)
+			.tickFormat('');
+		const yAxis = d3.axisLeft(y)
+			.ticks(5)
+		xAxisGroup.call(xAxis);
+		yAxisGroup.call(yAxis);
+		xAxisGroup.selectAll('text')
+			.attr('transform', 'rotate(-48)')
+			.attr('text-anchor', 'end')
+			.attr('fill', 'grey');
+	})
+}
+
+
+drawGraph('cases', 'total_cases');
+drawGraph('deaths', 'deaths');
+drawGraph('hospital', 'hospitalizations');
+drawGraph('testing', 'total_tests');
+
+
